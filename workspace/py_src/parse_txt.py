3812262d909cff75317acdc7a1355c4b636b4715
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-08-05 22:56:25 +0100

    added python to preprocess for nn

---
+++ workspace/py_src/parse_txt.py
@@ -0,0 +1,26 @@
+import os
+
+
+class_names = ['gemm', 'gemver', 'gesummv', 'symm', 'syr2k', 'syrk', 'trmm',
+               '2mm', '3mm', 'atax', 'bicg', 'doitgen', 'mvt', 'cholesky',
+               'durbin', 'gramschmidt', 'lu', 'ludcmp', 'trisolv', 'deriche',
+               'floyd-warshall', 'nussinov', 'adi', 'fdtd-2d', 'heat-3d',
+               'jacobi-1d', 'jacobi-2d', 'seidel-2d']
+def main():
+    parent_dir = '../../poly-e2/'
+    for root, dirs, files in os.walk(parent_dir):
+        for fd in files:
+            for item in class_names:
+                if (item in root) and (fd.endswith('.txt')):
+                    print(root, fd)
+
+# def parse_under_dir():
+def get_performance():
+def get_passlist():
+    pass
+def get_IRInfo():
+    pass
+
+
+if __name__ == "__main__":
+    main()

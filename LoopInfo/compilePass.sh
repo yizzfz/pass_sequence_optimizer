6ee4ce6011dc5836eaf83effe9d91252e74beb59
yizzfz <hc13414@my.bristol.ac.uk>
2017-08-28 10:16:53 +0100

    make script

--- LoopInfo/compilePass.sh
+++ LoopInfo/compilePass.sh
@@ -1,4 +1,1 @@
-cd myPass
-cmake . -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-Wno-unknown-warning-option -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_POLICY_DEFAULT_CMP0057=NEW
-ninja
-
+cmake . -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-Wno-unknown-warning-option -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_DEFAULT_CMP0057=NEW

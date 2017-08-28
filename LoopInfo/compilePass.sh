e28a3fa6f6a430afae8d7688a4886a99da4fcba7
yizzfz <hc13414@my.bristol.ac.uk>
2017-08-28 10:38:26 +0100

    make script

--- LoopInfo/compilePass.sh
+++ LoopInfo/compilePass.sh
@@ -1,1 +1,3 @@
+rm -rf CMakeCache.txt
+rm -rf CMakeFiles
 cmake . -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-Wno-unknown-warning-option -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_DEFAULT_CMP0057=NEW

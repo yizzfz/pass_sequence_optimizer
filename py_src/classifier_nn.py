9caa28ea2e56bdb4988a21df45d2f3753a1f207d
Han <hancui9140@qq.com>
2018-06-18 23:55:07 +0800

    data before wash

--- py_src/classifier_nn.py
+++ py_src/classifier_nn.py
@@ -10,6 +10,9 @@ import numpy as np
 import pickle
 import pdb
 
+printstr1 = ''
+printstr2 = ''
+printstr3 = ''
 
 def main(args):
     data = Data()
@@ -27,11 +30,23 @@ def main(args):
         train(epoch, model, train_loader, args)
         test(model, eval_loader, args)
 
-    final_test(model, eval_loader, args)
+    # final_test(model, eval_loader, args)
 
+def printlog():
+    #\x1b[3A = move cursor up 3 lines
+    #\x1b[2K = clear stdout from cursor
+    print('\x1b[3A\x1b[2K\r',end='')
+    print(' '*100)
+    print(' '*100)
+    print(' '*100)
+    print('\x1b[3A\x1b[2K\r',end='')
+    print(printstr1)
+    print(printstr2)
+    print(printstr3)
 
 def train(epoch, model, train_loader, args):
-    optimizer = optim.SGD(model.parameters(), lr=args.lr)
+    global printstr1,printstr2,printstr3
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
     model.train()
     dtype = torch.FloatTensor
     criterion = torch.nn.CrossEntropyLoss()
@@ -51,15 +66,22 @@ def train(epoch, model, train_loader, args):
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+            printstr1 = 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data), len(train_loader.dataset),
+                        100. * batch_idx / len(train_loader), loss.item())
+            printlog()
 
-    print('Train Epoch: {} Acc: {:.1f}'.format(
-        epoch, 100.*correct/len(train_loader.dataset)))
-
+    #         print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t'.format(
+    #             epoch, batch_idx * len(data), len(train_loader.dataset),
+    #             100. * batch_idx / len(train_loader), loss.item()))
+    #
+    # print('Train Epoch: {} Acc: {:.1f}'.format(
+    #     epoch, 100.*correct/len(train_loader.dataset)))
+    printstr2 = 'Train Epoch: {} Acc: {:.1f}'.format(epoch, 100.*correct/len(train_loader.dataset))
+    printlog()
 
 def test(model, loader, args):
+    global printstr3
     model.eval()
     test_loss = 0
     correct = 0
@@ -77,9 +99,14 @@ def test(model, loader, args):
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     test_loss /= len(loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(loader.dataset),
-        100. * correct / len(loader.dataset)))
+    printstr3 = 'Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+       test_loss, correct, len(loader.dataset),
+       100. * correct / len(loader.dataset))
+    printlog()
+
+    #print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    #    test_loss, correct, len(loader.dataset),
+    #    100. * correct / len(loader.dataset)))
 
 def read_test_list(name='Data/test_idx.pkl'):
     with open(name, 'rb') as f:
@@ -120,9 +147,9 @@ def final_test(model, loader, args):
         res.append(r)
 
 
-    with open('Data/result.pkl', 'wb') as f:
-        pickle.dump(res, f)
-        f.close()
+    # with open('Data/result.pkl', 'wb') as f:
+    #     pickle.dump(res, f)
+    #     f.close()
 
 
 
@@ -160,38 +187,47 @@ class Data(object):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(310, 500)
-        self.fc2 = nn.Linear(500, 1000)
-        self.fc3 = nn.Linear(1000, 200)
-        self.fc4 = nn.Linear(200, 100)
-        self.fc5 = nn.Linear(100, 3)
+        self.fc1 = nn.Linear(310, 200)
+        self.fc2 = nn.Linear(200, 20)
+        self.fc3 = nn.Linear(20, 3)
 
     def forward(self, x):
+
+        #print(x.cpu().detach().numpy())
+        self.checkNaN(x,'0')
+
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        x = F.dropout(x, training=self.training)
         x = self.fc3(x)
-        x = F.dropout(x, training=self.training)
-        x = self.fc4(x)
-        x = self.fc5(x)
+
+        self.checkNaN(x,'5')
+
+        # state = self.state_dict()
+        # print(state['fc1.weight'])
+
         return F.log_softmax(x, dim=1)
+
+    def checkNaN(self, x, s=''):
+        if(np.any(np.isnan(x.cpu().detach().numpy())) ):
+            print('Found nan', s)
+            exit(1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Pass sequence classifier')
     parser.add_argument(
-        '--batch-size', type=int, default=32, metavar='N',
+        '--batch-size', type=int, default=64, metavar='N',
         help='input batch size for training (default: 64)')
     parser.add_argument(
         '--test-batch-size', type=int, default=1000, metavar='N',
         help='input batch size for testing (default: 1000)')
     parser.add_argument(
-        '--epochs', type=int, default=500, metavar='N',
+        '--epochs', type=int, default=800, metavar='N',
         help='number of epochs to train (default: 10)')
     parser.add_argument(
-        '--lr', type=float, default=0.1, metavar='LR',
-        help='learning rate (default: 0.01)')
+        '--lr', type=float, default=0.001, metavar='LR',
+        help='learning rate (default: 0.1)')
     parser.add_argument(
         '--momentum', type=float, default=0.5, metavar='M',
         help='SGD momentum (default: 0.5)')
@@ -206,4 +242,5 @@ if __name__ == "__main__":
     args.cuda = torch.cuda.is_available()
     if(args.cuda):
         print('using CUDA')
+    print('learning rate', args.lr,'\n\n\n')
     main(args)
--- py_src/data_wash.py
+++ py_src/data_wash.py
@@ -4,21 +4,59 @@ import sys
 import itertools
 import pdb
 import numpy as np
+from sklearn.cluster import KMeans
 
 test_M=[2, 9, 11, 25, 22, 30, 37, 45, 52, 33]
-test_random=True
+test_random=1
 
 class Data_wash(object):
     def __init__(self):
-        self.read_file()
+        self.read_data_file()
         self.parse()
 
-    def read_file(self, name='data.pkl'):
+    def read_data_file(self, name='data.pkl'):
         with open(name, 'rb') as f:
             self.raw_data = pickle.load(f)
             self.list_data_type = pickle.load(f)
             self.list_pass = pickle.load(f)
             self.list_metric = pickle.load(f)
+
+    def read_label_file(self, name='cross.pkl'):
+        with open(name, 'rb') as f:
+            name = pickle.load(f)
+            tb = pickle.load(f)
+
+        name = ['../'+n[n.find('workspace'):] for n in name]
+
+        assert(len(name)==len(self.raw_data))
+        self.cross_tb = [[-255 for _ in range(0, len(name))] for _ in range(0, len(name))]
+        for i in range(0, len(name)):
+            for j in range(0, len(name)):
+                try:
+                    ii = name.index(self.paths[i])
+                    jj = name.index(self.paths[j])
+                    self.cross_tb[i][j] = tb[ii][jj]*100
+
+                except ValueError:
+                    print('Name not match')
+                    exit(1)
+
+
+        for i in range(0, len(self.raw_data)):
+            s = self.similar_programs[i]
+            tb = []+self.cross_tb[i]
+            tb.sort(reverse=1)
+            for idx,(pid,v) in enumerate(s):
+                assert(v[:8]==str(tb[idx])[:8])
+
+        print('ALL', len(name)*len(name))
+        for i in range(-20,15,5):
+            j=i+5
+            print('Range [',i,',\t',j,'\t]:\t', len([v for r in self.cross_tb for v in r if v>i and v<=j]))
+        print()
+
+
+
 
     def write_file(self):
         with open('Data/train_input.pkl', 'wb') as f1:
@@ -53,6 +91,7 @@ class Data_wash(object):
         self.GA_sequence = []
         self.similar_programs = []
         self.inputs = []
+        self.path_dict = dict()
         # flatten array lambda func
         flatten = lambda l: [item for sublist in l for item in sublist]
 
@@ -76,6 +115,10 @@ class Data_wash(object):
                 self.GA_sequence.append([])
             self.similar_programs.append(item[5])
 
+        self.read_label_file()
+
+
+
         """
         NN input = (N^N, M*2), NN output = (N^N, 3)
         N = number of programs = 55
@@ -91,7 +134,7 @@ class Data_wash(object):
         if(test_random==False):
             self.test_data_list = test_M
         else:
-            self.test_data_list = [random.randint(0, len(self.raw_data) - 1) for i in range (0, 6)]
+            self.test_data_list = [random.randint(0, len(self.raw_data) - 1) for i in range (0, 8)]
             self.test_data_list = list(set(self.test_data_list))
 
         print('selected test program:')
@@ -107,10 +150,12 @@ class Data_wash(object):
 
 
         if(np.any(np.isnan(self.train_input)) or np.any(np.isnan(self.test_input)) ):
-            print('nan')
+            print('Error: nan')
+            exit(1)
 
         if(np.any(np.isinf(self.train_input)) or np.any(np.isinf(self.test_input)) ):
-            print('inf')
+            print('Error: inf')
+            exit(1)
 
         self.write_file()
 
@@ -120,12 +165,29 @@ class Data_wash(object):
         self.train_input=np.zeros((size_train_data, self.M*2))
         self.train_label=np.zeros((size_train_data, 3))
         idx=0
+        res = [0, 0, 0, 0, 0, 0]
         for i in range(0, n):
             for j in range(0, n):
-                if(i!=j and not i in self.test_data_list and not j in self.test_data_list):
-                    self.train_input[idx]=self.inputs[i]+self.inputs[j]
-                    self.train_label[idx]=self.rank_similarity(i, j)
-                    idx=idx+1
+                if(i!=j and not i in self.test_data_list and not j in self.test_data_list
+                    and self.cross_tb[i][j]!=-255):
+                    data = self.inputs[i]+self.inputs[j]
+                    label = self.rank_similarity(i, j)
+
+                    for ii, v in enumerate(label):
+                        if v==1:
+                            #cut training data here?
+                            #if res[ii]<3000:
+                            if 1:
+                                res[ii]+=1
+                                self.train_input[idx]= data
+                                self.train_label[idx] = label
+                                idx+=1
+                            break
+
+        self.train_input=self.train_input[:idx][:]
+        self.train_label=self.train_label[:idx][:]
+        print('Training data distribution', res)
+
 
 
     def generate_test_data(self):
@@ -135,29 +197,75 @@ class Data_wash(object):
         self.test_input=np.zeros((size_test_data, self.M*2))
         self.test_label=np.zeros((size_test_data, 3))
         idx = 0
+        res = [0, 0, 0, 0, 0, 0]
         for i in self.test_data_list:
             for j in range(0, n):
-                if(i!=j):
-                    self.test_input[idx]=self.inputs[i]+self.inputs[j]
-                    self.test_label[idx]=self.rank_similarity(i, j)
-                    idx=idx+1
+                if(i!=j and self.cross_tb[i][j]!=-255):
+                    self.test_input[idx] = self.inputs[i]+self.inputs[j]
+                    tmp = self.rank_similarity(i, j)
+                    self.test_label[idx] = tmp
+
+                    for ii, v in enumerate(tmp):
+                        if v==1:
+                            res[ii]+=1
+                            break
+
+                    idx+=1
+        self.test_input=self.test_input[:idx][:]
+        self.test_label=self.test_label[:idx][:]
+        print('Test data distribution', res)
 
 
 
     def rank_similarity(self, i, j):
-        similar_of_i=self.similar_programs[i]
-        for (program_id, improvement) in similar_of_i:
-            #if 2nd program appears in 1st program's similar_programs list
-            #i.e. OPT(2) is better than O3(1)
-            if(program_id==j):
-                #if OPT(2) is better than O3(1) by more than 10%
-                if((float)(improvement)>10):
-                    return (1, 0, 0)
-                #if OPT(2) is better than O3(1) by less than 10%
-                else:
-                    return (0, 1, 0)
-        #if OPT(2) is worse than O3(1)
-        return (0, 0, 1)
+        # similar_of_i=self.similar_programs[i]
+        # res = None
+        # for (program_id, improvement) in similar_of_i:
+        #     #if 2nd program appears in 1st program's similar_programs list
+        #     #i.e. OPT(2) is better than O3(1)
+        #     if(program_id==j):
+        #         #if OPT(2) is better than O3(1) by more than 10%
+        #         if((float)(improvement)>10):
+        #             res = (1, 0, 0)
+        #         #if OPT(2) is better than O3(1) by less than 10%
+        #         else:
+        #             res = (0, 1, 0)
+        #
+        #
+        # #if OPT(2) is worse than O3(1)
+        # if res == None:
+        #     res = (0, 0, 1)
+        #
+        #     if self.cross_tb[i][j]>0:
+        #         print('i',self.benchmark_names[i])
+        #         print('j',self.benchmark_names[j])
+        #         print('similar of i',similar_of_i,'j',j,'not found')
+        #         print('tb',self.cross_tb[i][j])
+        #         input()
+        #
+        #
+        # if res == (0,0,1) and self.cross_tb[i][j]>0:
+        #     print('!',self.cross_tb[i][j], res)
+
+        v = self.cross_tb[i][j]
+
+        '''
+        Three class:
+            +2.5% better than O3
+            +2.5% - -2.5% not sure
+            -2.5% worse than O3
+        '''
+        if v>2.5:
+            return 1,0,0
+        if v<=2.5 and v>-2.5:
+            return 0,1,0
+        if v<=-2.5:
+            return 0,0,1
+
+        else:
+            print('Error in rank_similarity')
+            exit(1)
+
 
 
     def parse_ir_info_O3(self, raw_data):
